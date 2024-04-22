6581a365beb9c6840386af463fd6d90a99f59990
rosaTome <tomerosalinda@gmail.com>
2024-04-22 23:37:35 +0100

    grid templates used to style site

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import React from "react";
 import BeerContainer from "./containers/BeerContainer/BeerContainer";
 import beers from "./data/beers";
-
 import "./App.scss"
+import NavContainer from "./containers/NavContainer/NavContainer";
 
 const App = () => {
-
-
-
   return (
     <div className="app">
       <h1 className="app-heading">Beer O'clock</h1>
-      <BeerContainer beerCards={beers} />
+      <NavContainer
+      />
 
-        
+      <BeerContainer 
+      beerCards={beers} 
+      />
     </div>
+
+
   )
 }
 
---
+++ src/containers/NavContainer/NavContainer.tsx
@@ -0,0 +1,22 @@
+import "./NavContainer.scss"
+
+type NavContainerProps = {
+
+}
+
+const NavContainer = ({}: NavContainerProps) => {
+
+    return (
+        <nav className="nav-container">
+
+            {/* SearchBox */}
+            <p>Search Box:</p>
+
+            {/* FiltersList */}
+            <p>Filters</p>
+
+        </nav>
+    )
+}
+
+export default NavContainer;
